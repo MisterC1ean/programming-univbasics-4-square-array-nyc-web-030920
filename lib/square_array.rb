16c9fb79a1e99d9ade7ca076678f13766165ee3d
Robert Keller <robert_keller44@icloud.com>
2019-12-24 02:36:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   counter = 0
  
   while array[counter] do
-    array[counter] * array[counter]
+    new_array << (array[counter] * array[counter])
     counter += 1
-    array
   end
+  new_array
 end
